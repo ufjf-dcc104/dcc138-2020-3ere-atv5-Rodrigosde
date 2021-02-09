c8e58f6369688d7233d3f3565108817f2c93d203
Rodrigo detoni <rodrigodeton@gmail.com>
2021-02-09 17:39:40 -0300

    adiciona sprite com construtor com parametros

--- js/Cena.js
+++ js/Cena.js
@@ -1,12 +1,12 @@
 export default class Cena {
-    //responsável por desenhar elementos na tela em uma animacao
+    //responsável por desenhar elementos na tela em uma animação
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
     }
     desenhar(){
-        this.fillStyle = "grey";
+        this.ctx.fillStyle = "grey";
         this.ctx.fillRect(0, 0, this.canvas.width,
-             this.canvas.height);
+        this.canvas.height);
     }
 }
