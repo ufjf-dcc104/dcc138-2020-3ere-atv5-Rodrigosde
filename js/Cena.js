105e89b1eeecc9a6c7c6d19d9cea761ac36d9833
Rodrigo detoni <rodrigodeton@gmail.com>
2021-03-19 18:32:24 -0300

    formata codigo

--- js/Cena.js
+++ js/Cena.js
@@ -1,113 +1,106 @@
 import Sprite from "./Sprite.js";
 
 export default class Cena {
-    //responsável por desenhar elementos na tela em uma animação
-    constructor(canvas = null, assets = null){
-        this.canvas = canvas;
-        this.ctx = canvas?.getContext("2d");
-        this.assets = assets;
-        this.game = null;
-        this.preparar();
+  //responsável por desenhar elementos na tela em uma animação
+  constructor(canvas = null, assets = null) {
+    this.canvas = canvas;
+    this.ctx = canvas?.getContext("2d");
+    this.assets = assets;
+    this.game = null;
+    this.preparar();
+  }
+  desenhar() {
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.mapa?.desenhar(this.ctx);
+    if (this.assets.acabou()) {
+      for (let s = 0; s < this.sprites.length; s++) {
+        const sprite = this.sprites[s];
+        sprite.aplicaRestricoes();
+        sprite.desenhar(this.ctx);
+      }
     }
-    desenhar(){
-       
-        this.ctx.fillRect(0, 0, this.canvas.width,
-        this.canvas.height);
-        this.mapa?.desenhar(this.ctx);
-        if (this.assets.acabou()) {
-            for (let s = 0; s < this.sprites.length; s++) {
-                const sprite = this.sprites[s];
-                sprite.aplicaRestricoes();
-                sprite.desenhar(this.ctx);
-            }
-        }
-        this.ctx.fillStyle= "white";
-        this.ctx.fillText(this.assets?.progresso(), 10, 20);
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText(this.assets?.progresso(), 10, 20);
+  }
+  passo(dt) {
+    if (this.assets.acabou()) {
+      for (const sprite of this.sprites) {
+        sprite.passo(dt);
+      }
     }
-    passo(dt){
-        if (this.assets.acabou()) {
-            for (const sprite of this.sprites) {
-                sprite.passo(dt);
-            }
-        }
-    }
-    quadro(t){
-        this.t0 = this.t0 ?? t;
-        this.dt = (t - this.t0)/1000;
+  }
+  quadro(t) {
+    this.t0 = this.t0 ?? t;
+    this.dt = (t - this.t0) / 1000;
 
-        this.passo(this.dt);
-        this.desenhar();
-        this.checaColisao();
-        this.removerSprites();
+    this.passo(this.dt);
+    this.desenhar();
+    this.checaColisao();
+    this.removerSprites();
 
-        if (this.rodando) {
-            this.iniciar();    
+    if (this.rodando) {
+      this.iniciar();
+    }
+    this.t0 = t;
+  }
+  iniciar() {
+    this.rodando = true;
+    this.idAnim = requestAnimationFrame((t) => {
+      this.quadro(t);
+    });
+  }
+  parar() {
+    this.rodando = false;
+    cancelAnimationFrame(this.idAnim);
+    this.t0 = null;
+    this.dt = 0;
+  }
+  checaColisao() {
+    for (let a = 0; a < this.sprites.length - 1; a++) {
+      const spriteA = this.sprites[a];
+      for (let b = a + 1; b < this.sprites.length; b++) {
+        const spriteB = this.sprites[b];
+        if (spriteA.colidiuCom(spriteB)) {
+          this.quandoColidir(spriteA, spriteB);
         }
-        this.t0 = t;
+      }
     }
-    iniciar(){
-        this.rodando = true;
-        this.idAnim = requestAnimationFrame((t) => {
-                this.quadro(t);
-            }
-        );
+  }
+  quandoColidir(a, b) {
+    if (!this.aRemover.includes(a)) {
+      this.aRemover.push(a);
     }
-    parar(){
-        this.rodando = false;
-        cancelAnimationFrame(this.idAnim);
-        this.t0 = null;
-        this.dt = 0;
+    if (!this.aRemover.includes(b)) {
+      this.aRemover.push(b);
     }
-    checaColisao(){
-        for (let a = 0; a < this.sprites.length-1; a++) {
-            const spriteA = this.sprites[a];
-            for (let b = a+1; b < this.sprites.length; b++) {
-                const spriteB = this.sprites[b];
-                if (spriteA.colidiuCom(spriteB)) {
-                    this.quandoColidir(spriteA, spriteB);
-                   
-                }         
-            }    
-        }
+    this.assets.play("boom");
+  }
+  removerSprites() {
+    for (const alvo of this.aRemover) {
+      const idx = this.sprites.indexOf(alvo);
+      if (idx >= 0) {
+        this.sprites.splice(idx, 1);
+      }
     }
-    quandoColidir(a,b){
-        
-        if (!this.aRemover.includes(a)) {
-            this.aRemover.push(a);
-        }
-        if (!this.aRemover.includes(b)) {
-            this.aRemover.push(b);
-        }
-       this.assets.play("boom");
-    }
-    removerSprites(){
-        
-        for (const alvo of this.aRemover) {
-            const idx = this.sprites.indexOf(alvo);
-            if (idx >= 0) {
-                this.sprites.splice(idx, 1);
-            }
-        }
-        this.aRemover = [];    
-    }
-    configuraMapa(mapa){
-        this.mapa = mapa;
-        this.mapa.cena = this;
-    }
+    this.aRemover = [];
+  }
+  configuraMapa(mapa) {
+    this.mapa = mapa;
+    this.mapa.cena = this;
+  }
 
-    adicionar(sprite){
-        sprite.cena = this;
-        this.sprites.push(sprite);
-    }
+  adicionar(sprite) {
+    sprite.cena = this;
+    this.sprites.push(sprite);
+  }
 
-   
-    preparar(){
-        this.sprites = [];
-        this.aRemover = [];
-        this.t0  = null;
-        this.dt = 0;
-        this.idAnim = null;
-        this.mapa = null;
-        this.rodando = true;
-    }
+  preparar() {
+    this.sprites = [];
+    this.aRemover = [];
+    this.t0 = null;
+    this.dt = 0;
+    this.idAnim = null;
+    this.mapa = null;
+    this.rodando = true;
+  }
 }
